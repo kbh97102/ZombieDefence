15082bf070ef0bfb9d24f490035fc77c7fd7a618
Arakene <arakene_dev@naver.com>
2021-05-22 18:31:51 +0900

    Add gun asset

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,11 @@
 using System;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float m_moveSpeed = 2.0f;
-
+    public Camera mainCamera;
     private Animator _animator;
 
     private void Start()
@@ -15,6 +16,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         PlayerMove();
+        ChasingCursor();
     }
 
     void PlayerMove()
@@ -35,8 +37,21 @@ public class PlayerController : MonoBehaviour
         Vector3 moveVertical = Vector3.forward * v;
         Vector3 velocity = (moveHorizontal + moveVertical).normalized;
 
-        transform.LookAt(transform.position + velocity);
-
         transform.Translate(velocity * m_moveSpeed * Time.deltaTime, Space.World);
+    }
+
+    private void ChasingCursor()
+    {
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+ 
+        if (groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.cyan);
+ 
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
     }
 }
