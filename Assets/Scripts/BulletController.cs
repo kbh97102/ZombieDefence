abb0019e1d5258b5386d62ad43f6b254f6bd76ae
Arakene <arakene_dev@naver.com>
2021-05-27 14:03:53 +0900

    Update bullet

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -1,18 +1,17 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Numerics;
 using UnityEngine;
-using Vector3 = UnityEngine.Vector3;
 
 public class BulletController : MonoBehaviour
 {
-
     #region Unity
-    
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+        }
+
+        if (other.CompareTag("Object"))
         {
             Destroy(gameObject);
         }
@@ -23,7 +22,6 @@ public class BulletController : MonoBehaviour
     public void shooting(Vector3 vector3)
     {
         Destroy(gameObject, 30f);
-        GetComponent<Rigidbody>().AddForce(vector3*100);
+        GetComponent<Rigidbody>().AddForce(vector3 * 1500);
     }
-    
 }
