d2acdfbff2f64209986d1c1b2a71bd8fcaced362
Arakene <arakene_dev@naver.com>
2021-05-28 15:51:44 +0900

    Update help me text

--- Assets/Scripts/StartScene/StartUI.cs
+++ Assets/Scripts/StartScene/StartUI.cs
@@ -1,14 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class StartUI : MonoBehaviour
 {
-
     public void OnClickStartGame()
     {
         SceneManager.LoadScene("SampleScene");
-    } 
-    
+    }
+
+
+    public void OnClickHelp()
+    {
+        var text = "Press K -> stop bgm\n" +
+                   "Pree L -> play bgm";
+    }
 }
