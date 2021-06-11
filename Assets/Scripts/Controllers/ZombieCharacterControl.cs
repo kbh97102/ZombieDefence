cfe78157fbfd0a10952ab5be5495d9b42ddfcc6d
Arakene <arakene_dev@naver.com>
2021-06-11 21:17:35 +0900

    Remove unusedCode

--- Assets/Scripts/Controllers/ZombieCharacterControl.cs
+++ Assets/Scripts/Controllers/ZombieCharacterControl.cs
@@ -111,46 +111,46 @@ public class ZombieCharacterControl : MonoBehaviour
         // }
     }
 
-    private void TankUpdate()
-    {
-        float v = Input.GetAxis("Vertical");
-        float h = Input.GetAxis("Horizontal");
-
-        m_currentV = Mathf.Lerp(m_currentV, v, Time.deltaTime * m_interpolation);
-        m_currentH = Mathf.Lerp(m_currentH, h, Time.deltaTime * m_interpolation);
-
-        transform.position += transform.forward * m_currentV * m_moveSpeed * Time.deltaTime;
-        transform.Rotate(0, m_currentH * m_turnSpeed * Time.deltaTime, 0);
-
-        m_animator.SetFloat("MoveSpeed", m_currentV);
-    }
-
-    private void DirectUpdate()
-    {
-        float v = Input.GetAxis("Vertical");
-        float h = Input.GetAxis("Horizontal");
-
-        Transform camera = Camera.main.transform;
-
-        m_currentV = Mathf.Lerp(m_currentV, v, Time.deltaTime * m_interpolation);
-        m_currentH = Mathf.Lerp(m_currentH, h, Time.deltaTime * m_interpolation);
-
-        Vector3 direction = camera.forward * m_currentV + camera.right * m_currentH;
-
-        float directionLength = direction.magnitude;
-        direction.y = 0;
-        direction = direction.normalized * directionLength;
-
-        if (direction != Vector3.zero)
-        {
-            m_currentDirection = Vector3.Slerp(m_currentDirection, direction, Time.deltaTime * m_interpolation);
-
-            transform.rotation = Quaternion.LookRotation(m_currentDirection);
-            transform.position += m_currentDirection * m_moveSpeed * Time.deltaTime;
-
-            m_animator.SetFloat("MoveSpeed", direction.magnitude);
-        }
-    }
+    // private void TankUpdate()
+    // {
+    //     float v = Input.GetAxis("Vertical");
+    //     float h = Input.GetAxis("Horizontal");
+    //
+    //     m_currentV = Mathf.Lerp(m_currentV, v, Time.deltaTime * m_interpolation);
+    //     m_currentH = Mathf.Lerp(m_currentH, h, Time.deltaTime * m_interpolation);
+    //
+    //     transform.position += transform.forward * m_currentV * m_moveSpeed * Time.deltaTime;
+    //     transform.Rotate(0, m_currentH * m_turnSpeed * Time.deltaTime, 0);
+    //
+    //     m_animator.SetFloat("MoveSpeed", m_currentV);
+    // }
+    //
+    // private void DirectUpdate()
+    // {
+    //     float v = Input.GetAxis("Vertical");
+    //     float h = Input.GetAxis("Horizontal");
+    //
+    //     Transform camera = Camera.main.transform;
+    //
+    //     m_currentV = Mathf.Lerp(m_currentV, v, Time.deltaTime * m_interpolation);
+    //     m_currentH = Mathf.Lerp(m_currentH, h, Time.deltaTime * m_interpolation);
+    //
+    //     Vector3 direction = camera.forward * m_currentV + camera.right * m_currentH;
+    //
+    //     float directionLength = direction.magnitude;
+    //     direction.y = 0;
+    //     direction = direction.normalized * directionLength;
+    //
+    //     if (direction != Vector3.zero)
+    //     {
+    //         m_currentDirection = Vector3.Slerp(m_currentDirection, direction, Time.deltaTime * m_interpolation);
+    //
+    //         transform.rotation = Quaternion.LookRotation(m_currentDirection);
+    //         transform.position += m_currentDirection * m_moveSpeed * Time.deltaTime;
+    //
+    //         m_animator.SetFloat("MoveSpeed", direction.magnitude);
+    //     }
+    // }
 
     public void SetTarget(GameObject target)
     {
