5e32de124af197f4d15bbc66839282e359333aed
Arakene <arakene_dev@naver.com>
2021-06-11 22:47:08 +0900

    Update User Nick Name change

---
+++ Assets/Scripts/UI/NickNameInput.cs
@@ -0,0 +1,39 @@
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(InputField))]
+public class NickNameInput : MonoBehaviour
+{
+    const string playerNamePrefKey = "PlayerName";
+
+    void Start()
+    {
+        string defaultName = string.Empty;
+        InputField _inputField = this.GetComponent<InputField>();
+        if (_inputField != null)
+        {
+            if (PlayerPrefs.HasKey(playerNamePrefKey))
+            {
+                defaultName = PlayerPrefs.GetString(playerNamePrefKey);
+                _inputField.text = defaultName;
+            }
+        }
+
+        PhotonNetwork.NickName = defaultName;
+    }
+
+    public void SetPlayerName(string value)
+    {
+        if (string.IsNullOrEmpty(value))
+        {
+            Debug.LogError("Player Name is null or empty");
+            return;
+        }
+
+        PhotonNetwork.NickName = value;
+
+
+        PlayerPrefs.SetString(playerNamePrefKey, value);
+    }
+}
